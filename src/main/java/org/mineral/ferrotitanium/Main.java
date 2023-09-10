fc42a6ed3267579e1c4098c53238ada89c15ab14
SharwOrange <kuliangcha@qq.com>
2023-09-10 17:25:09 +0800

    引入使用到的库

--- src/main/java/org/mineral/ferrotitanium/Main.java
+++ src/main/java/org/mineral/ferrotitanium/Main.java
@@ -2,6 +2,11 @@ package org.mineral.ferrotitanium;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
